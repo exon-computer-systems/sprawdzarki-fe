971e12b7192054de1696fd7049efc0e087259c3c
Szymon <szmn.rtn@gmail.com>
2024-09-19 11:57:17 +0200

    Finish slider logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,109 @@
 import Slider from "./components/slider/Slider";
 import "./App.css";
+import axios from "axios";
+import Carousel from "./components/carousel/Carousel";
+
+import { slides } from "./data/carouselData.json";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  return (
-    <div>
-      <Slider />
-    </div>
-  );
+    const [myPlaylist, setMyPlaylist] = useState({});
+
+    const increaseStats = async (materialId, postId, updatedData) => {
+        try {
+            const materialResponse = await axios.get(
+                `http://http://192.168.68.172:8000/api/materials/${materialId}?populate=posts`
+            );
+            const materialData = materialResponse.data.data.attributes;
+
+            const updatedPosts = materialData.posts.map((post) => {
+                if (post.id === postId) {
+                    return {
+                        ...post,
+                        playsCount:
+                            post.playsCount + (updatedData.playsCount || 0),
+                        playsTime:
+                            post.playsTime + (updatedData.playsTime || 0),
+                        fullPlaysCount:
+                            post.fullPlaysCount +
+                            (updatedData.fullPlaysCount || 0),
+                    };
+                }
+                return post;
+            });
+
+            const res = await axios.put(
+                `http://http://192.168.68.172:1337/api/materials/${materialId}`,
+                {
+                    data: {
+                        posts: updatedPosts,
+                    },
+                }
+            );
+
+            console.log(res);
+        } catch (err) {
+            console.warn(err);
+        } finally {
+        }
+    };
+
+    useEffect(() => {
+        const getPosts = async () => {
+            try {
+                const res = await axios.get(
+                    "http://192.168.68.172:8000/api/materials?populate=posts.media"
+                );
+
+                if (res) {
+                    const activeItems = res.data.data.find(
+                        (item) => item.attributes.isActive === true
+                    );
+
+                    console.log("Active Items:", activeItems);
+
+                    if (activeItems) {
+                        setMyPlaylist(activeItems); // Ustawienie stanu
+                    }
+                }
+            } catch (err) {
+                console.warn(err);
+            }
+        };
+
+        getPosts();
+    }, []);
+
+    return (
+        <div className="app">
+            {/* <Slider /> */}
+            {myPlaylist?.attributes?.posts ? (
+                <Carousel data={slides} posts={myPlaylist.attributes.posts} />
+            ) : (
+                // <p>lol</p>
+                <p>Loading</p>
+            )}
+
+            {/* <button
+                style={{
+                    position: "absolute",
+                    bottom: "20px",
+                    left: "50%",
+                    width: "200px",
+                    height: "50px",
+                }}
+                onClick={() =>
+                    increaseStats(1, 10, {
+                        playsCount: 1,
+                        playsTime: 10,
+                        fullPlaysCount: 1,
+                    })
+                }
+            >
+                Test
+            </button> */}
+        </div>
+    );
 };
 
 export default App;
---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,129 @@
+import React, { useEffect, useRef, useState } from "react";
+import styles from "./Carousel.module.css";
+
+const Carousel = ({ data, posts }) => {
+    const [playlistData, setPlaylistData] = useState("");
+    const [slide, setSlide] = useState(0);
+    const [playStartTime, setPlayStartTime] = useState(Date.now());
+
+    const videoRef = useRef([]);
+    const timeoutRef = useRef(0);
+
+    useEffect(() => {
+        if (!playlistData) return;
+
+        if (playlistData.length) {
+            getSpentTime();
+        }
+
+        const currentSlideDuration = playlistData[slide]?.duration * 1000;
+
+        if (playlistData[slide]?.isVideo && videoRef.current[slide]) {
+            videoRef.current[slide].currentTime = 0;
+            videoRef.current[slide].play();
+        }
+
+        timeoutRef.current = setTimeout(() => {
+            setSlide(slide === playlistData.length - 1 ? 0 : slide + 1);
+        }, currentSlideDuration);
+
+        return () => clearTimeout(timeoutRef.current);
+    }, [slide, playlistData]);
+
+    useEffect(() => {
+        // console.log(posts);
+
+        if (posts) {
+            setPlaylistData(posts);
+        }
+    }, [posts]);
+
+    const getSpentTime = async () => {
+        const currentTime = Date.now();
+        const timeSpent = currentTime - playStartTime;
+
+        let i = slide - 1 === -1 ? playlistData.length - 1 : slide - 1;
+
+        if (timeSpent >= 1000) {
+            console.log({
+                playCount: 1,
+                playTime: (timeSpent / 1000).toFixed(1),
+                fullPlayCount:
+                    playlistData[i].duration * 0.9 * 1000 > timeSpent ? 0 : 1,
+            });
+        }
+
+        setPlayStartTime(currentTime);
+    };
+
+    const handleEnd = () => {
+        clearTimeout(timeoutRef.current);
+        setSlide(slide === playlistData.length - 1 ? 0 : slide + 1);
+    };
+
+    return (
+        <>
+            <section
+                className={styles.carousel}
+                // onClick={() => handleEnd()}
+            >
+                {playlistData ? (
+                    playlistData.map((el, idx) => {
+                        return !el.isVideo ? (
+                            <img
+                                key={el.id}
+                                src={el.media.data.attributes.url}
+                                alt={el.title}
+                                className={
+                                    slide === idx
+                                        ? styles.slide
+                                        : `${styles.slide} ${styles.slide_hidden}`
+                                }
+                            />
+                        ) : (
+                            <video
+                                key={el.id}
+                                className={
+                                    slide === idx
+                                        ? styles.slide
+                                        : `${styles.slide} ${styles.slide_hidden}`
+                                }
+                                muted
+                                // autoPlay
+                                ref={(el) => (videoRef.current[idx] = el)}
+                            >
+                                <source
+                                    src={el.media.data.attributes.url}
+                                    type="video/mp4"
+                                />
+                            </video>
+                        );
+                    })
+                ) : (
+                    <p>loading...</p>
+                )}
+
+                <span className={styles.indicators}>
+                    {playlistData &&
+                        playlistData.map((_, idx) => {
+                            return (
+                                <button
+                                    key={idx}
+                                    className={
+                                        slide === idx
+                                            ? styles.indicator
+                                            : `${styles.indicator} ${styles.indicator_inactive}`
+                                    }
+                                    onClick={() => {
+                                        setSlide(idx);
+                                    }}
+                                ></button>
+                            );
+                        })}
+                </span>
+            </section>
+        </>
+    );
+};
+
+export default Carousel;
